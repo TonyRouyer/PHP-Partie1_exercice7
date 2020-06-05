68e5ee0319ec49d61915b34ab98a897f539e0036
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-05 15:00:38 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 1 exercice 7</title>
+</head>
+<body>
+    <p>
+        <?php
+            $firstName = 'Tony';
+            $lastName = 'Rouyer';
+            $age = 24;
+            $result = 'Bonjour ' . $lastName . ' ' . $firstName . ', tu as ' . $age . 'ans.';
+            echo $result;
+        ?>
+    </p>
+</body>
+</html>
